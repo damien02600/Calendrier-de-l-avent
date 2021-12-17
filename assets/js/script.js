0d772c848f327790b6fb0abf08bcf3aa0ffb5343
damien02600 <d.berthe1997@laposte.net>
2021-12-17 16:18:17 +0100

    Calendrier de l'avent

---
+++ assets/js/script.js
@@ -0,0 +1,345 @@
+let effect1 = document.getElementById("effect1");
+
+effect1.onclick = () => {
+  if (effect1.classList.contains("animate__flip")) {
+    effect1.setAttribute("src", "assets/img/PAGE RECTO 1.png");
+    effect1.classList.remove("animate__flip");
+    effect1.classList.add("animate__flip");
+  } else {
+    effect1.setAttribute("src", "assets/img/VERSO 1.png");
+    effect1.classList.remove("animate__flip");
+    effect1.classList.add("animate__flip");
+  }
+};
+
+let effect2 = document.getElementById("effect2");
+
+effect2.onclick = () => {
+  if (effect2.classList.contains("animate__flip")) {
+    effect2.setAttribute("src", "assets/img/PAGE RECTO 2.png");
+    effect2.classList.remove("animate__flip");
+    effect2.classList.add("animate__flip");
+  } else {
+    effect2.setAttribute("src", "assets/img/VERSO 2.png");
+    effect2.classList.remove("animate__flip");
+    effect2.classList.add("animate__flip");
+  }
+};
+
+let effect3 = document.getElementById("effect3");
+
+effect3.onclick = () => {
+  if (effect3.classList.contains("animate__flip")) {
+    effect3.setAttribute("src", "assets/img/PAGE RECTO 3.png");
+    effect3.classList.remove("animate__flip");
+    effect3.classList.add("animate__flip");
+  } else {
+    effect3.setAttribute("src", "assets/img/VERSO 3.png");
+    effect3.classList.remove("animate__flip");
+    effect3.classList.add("animate__flip");
+  }
+};
+
+let effect8 = document.getElementById("effect8");
+
+effect8.onclick = () => {
+  if (effect8.classList.contains("animate__flip")) {
+    effect8.setAttribute(
+      "src",
+      "assets/img/Nouveaux 8 et 11 recto verso/PAGE RECTO 8.png"
+    );
+    effect8.classList.remove("animate__flip");
+    effect8.classList.add("animate__flip");
+  } else {
+    effect8.setAttribute(
+      "src",
+      "assets/img/Nouveaux 8 et 11 recto verso/VERSO 8.png"
+    );
+    effect8.classList.remove("animate__flip");
+    effect8.classList.add("animate__flip");
+  }
+};
+
+let effect9 = document.getElementById("effect9");
+
+effect9.onclick = () => {
+  if (effect9.classList.contains("animate__flip")) {
+    effect9.setAttribute("src", "assets/img/PAGE RECTO 9.png");
+    effect9.classList.remove("animate__flip");
+    effect9.classList.add("animate__flip");
+  } else {
+    effect9.setAttribute("src", "assets/img/VERSO 9.png");
+    effect9.classList.remove("animate__flip");
+    effect9.classList.add("animate__flip");
+  }
+};
+
+let effect12 = document.getElementById("effect12");
+
+effect12.onclick = () => {
+  if (effect12.classList.contains("animate__flip")) {
+    effect12.setAttribute("src", "assets/img/PAGE RECTO 12.png");
+    effect12.classList.remove("animate__flip");
+    effect12.classList.add("animate__flip");
+  } else {
+    effect12.setAttribute("src", "assets/img/VERSO 12.png");
+    effect12.classList.remove("animate__flip");
+    effect12.classList.add("animate__flip");
+  }
+};
+
+let effect13 = document.getElementById("effect13");
+effect13.onclick = () => {
+  if (effect13.classList.contains("animate__flip")) {
+    effect13.setAttribute("src", "assets/img/PAGE RECTO 13.png");
+    effect13.classList.remove("animate__flip");
+    effect13.classList.add("animate__flip");
+  } else {
+    effect13.setAttribute("src", "assets/img/VERSO 13.png");
+    effect13.classList.remove("animate__flip");
+    effect13.classList.add("animate__flip");
+  }
+};
+
+let effect14 = document.getElementById("effect14");
+effect14.onclick = () => {
+  if (effect14.classList.contains("animate__flip")) {
+    effect14.setAttribute("src", "assets/img/PAGE RECTO 14.png");
+    effect14.classList.remove("animate__flip");
+    effect14.classList.add("animate__flip");
+  } else {
+    effect14.setAttribute("src", "assets/img/VERSO 14.png");
+    effect14.classList.remove("animate__flip");
+    effect14.classList.add("animate__flip");
+  }
+};
+
+let effect18 = document.getElementById("effect18");
+effect18.onclick = () => {
+  if (effect18.classList.contains("animate__flip")) {
+    effect18.setAttribute("src", "assets/img/PAGE RECTO 18.png");
+    effect18.classList.remove("animate__flip");
+    effect18.classList.add("animate__flip");
+  } else {
+    effect18.setAttribute("src", "assets/img/VERSO 18.png");
+    effect18.classList.remove("animate__flip");
+    effect18.classList.add("animate__flip");
+  }
+};
+
+let effect19 = document.getElementById("effect19");
+effect19.onclick = () => {
+  if (effect19.classList.contains("animate__flip")) {
+    effect19.setAttribute("src", "assets/img/PAGE RECTO 19.png");
+    effect19.classList.remove("animate__flip");
+    effect19.classList.add("animate__flip");
+  } else {
+    effect19.setAttribute("src", "assets/img/VERSO 19.png");
+    effect19.classList.remove("animate__flip");
+    effect19.classList.add("animate__flip");
+  }
+};
+
+let effect6 = document.getElementById("effect6");
+effect6.onclick = () => {
+  if (effect6.classList.contains("animate__flip")) {
+    effect6.setAttribute("src", "assets/img/PAGE RECTO 6.png");
+    effect6.classList.remove("animate__flip");
+    effect6.classList.add("animate__flip");
+  } else {
+    effect6.setAttribute("src", "assets/img/VERSO 6.png");
+    effect6.classList.remove("animate__flip");
+    effect6.classList.add("animate__flip");
+  }
+};
+
+let effect7 = document.getElementById("effect7");
+effect7.onclick = () => {
+  if (effect7.classList.contains("animate__flip")) {
+    effect7.setAttribute("src", "assets/img/PAGE RECTO 7.png");
+    effect7.classList.remove("animate__flip");
+    effect7.classList.add("animate__flip");
+  } else {
+    effect7.setAttribute("src", "assets/img/VERSO 7.png");
+    effect7.classList.remove("animate__flip");
+    effect7.classList.add("animate__flip");
+  }
+};
+
+let effect10 = document.getElementById("effect10");
+effect10.onclick = () => {
+  if (effect10.classList.contains("animate__flip")) {
+    effect10.setAttribute("src", "assets/img/PAGE RECTO 10.png");
+    effect10.classList.remove("animate__flip");
+    effect10.classList.add("animate__flip");
+  } else {
+    effect10.setAttribute("src", "assets/img/VERSO 10.png");
+    effect10.classList.remove("animate__flip");
+    effect10.classList.add("animate__flip");
+  }
+};
+
+let effect23 = document.getElementById("effect23");
+
+effect23.onclick = () => {
+  if (effect23.classList.contains("animate__flip")) {
+    effect23.setAttribute(
+      "src",
+      "assets/img/RECTO_200x200_600_x_200_px (23).png"
+    );
+    effect23.classList.remove("animate__flip");
+    effect23.classList.add("animate__flip");
+  } else {
+    effect23.setAttribute("src", "assets/img/VERSO 23.png");
+    effect23.classList.remove("animate__flip");
+    effect23.classList.add("animate__flip");
+  }
+};
+
+let effect15 = document.getElementById("effect15");
+
+effect15.onclick = () => {
+  if (effect15.classList.contains("animate__flip")) {
+    effect15.setAttribute("src", "assets/img/PAGE RECTO 15.png");
+    effect15.classList.remove("animate__flip");
+    effect15.classList.add("animate__flip");
+  } else {
+    effect15.setAttribute("src", "assets/img/VERSO 15.png");
+    effect15.classList.remove("animate__flip");
+    effect15.classList.add("animate__flip");
+  }
+};
+
+let effect20 = document.getElementById("effect20");
+
+effect20.onclick = () => {
+  if (effect20.classList.contains("animate__flip")) {
+    effect20.setAttribute("src", "assets/img/PAGE RECTO 20.png");
+    effect20.classList.remove("animate__flip");
+    effect20.classList.add("animate__flip");
+  } else {
+    effect20.setAttribute("src", "assets/img/VERSO 20.png");
+    effect20.classList.remove("animate__flip");
+    effect20.classList.add("animate__flip");
+  }
+};
+
+let effect21 = document.getElementById("effect21");
+
+effect21.onclick = () => {
+  if (effect21.classList.contains("animate__flip")) {
+    effect21.setAttribute("src", "assets/img/PAGE RECTO 21.png");
+    effect21.classList.remove("animate__flip");
+    effect21.classList.add("animate__flip");
+  } else {
+    effect21.setAttribute("src", "assets/img/VERSO 21.png");
+    effect21.classList.remove("animate__flip");
+    effect21.classList.add("animate__flip");
+  }
+};
+
+let effect22 = document.getElementById("effect22");
+
+effect22.onclick = () => {
+  if (effect22.classList.contains("animate__flip")) {
+    effect22.setAttribute("src", "assets/img/PAGE RECTO 22.png");
+    effect22.classList.remove("animate__flip");
+    effect22.classList.add("animate__flip");
+  } else {
+    effect22.setAttribute("src", "assets/img/VERSO 22.png");
+    effect22.classList.remove("animate__flip");
+    effect22.classList.add("animate__flip");
+  }
+};
+
+let effect5 = document.getElementById("effect5");
+
+effect5.onclick = () => {
+  if (effect5.classList.contains("animate__flip")) {
+    effect5.setAttribute("src", "assets/img/PAGE RECTO 5.png");
+    effect5.classList.remove("animate__flip");
+    effect5.classList.add("animate__flip");
+  } else {
+    effect5.setAttribute("src", "assets/img/VERSO 5.png");
+    effect5.classList.remove("animate__flip");
+    effect5.classList.add("animate__flip");
+  }
+};
+
+let effect4 = document.getElementById("effect4");
+
+effect4.onclick = () => {
+  if (effect4.classList.contains("animate__flip")) {
+    effect4.setAttribute("src", "assets/img/PAGE RECTO 4.png");
+    effect4.classList.remove("animate__flip");
+    effect4.classList.add("animate__flip");
+  } else {
+    effect4.setAttribute("src", "assets/img/VERSO 4.png");
+    effect4.classList.remove("animate__flip");
+    effect4.classList.add("animate__flip");
+  }
+};
+
+let effect11 = document.getElementById("effect11");
+
+effect11.onclick = () => {
+  if (effect11.classList.contains("animate__flip")) {
+    effect11.setAttribute(
+      "src",
+      "assets/img/Nouveaux 8 et 11 recto verso/PAGE RECTO 11.png"
+    );
+    effect11.classList.remove("animate__flip");
+    effect11.classList.add("animate__flip");
+  } else {
+    effect11.setAttribute(
+      "src",
+      "assets/img/Nouveaux 8 et 11 recto verso/VERSO 11.png"
+    );
+    effect11.classList.remove("animate__flip");
+    effect11.classList.add("animate__flip");
+  }
+};
+
+let effect16 = document.getElementById("effect16");
+
+effect16.onclick = () => {
+  if (effect16.classList.contains("animate__flip")) {
+    effect16.setAttribute("src", "assets/img/PAGE RECTO 16.png");
+    effect16.classList.remove("animate__flip");
+    effect16.classList.add("animate__flip");
+  } else {
+    effect16.setAttribute("src", "assets/img/VERSO 16.png");
+    effect16.classList.remove("animate__flip");
+    effect16.classList.add("animate__flip");
+  }
+};
+
+let effect17 = document.getElementById("effect17");
+
+effect17.onclick = () => {
+  if (effect17.classList.contains("animate__flip")) {
+    effect17.setAttribute("src", "assets/img/PAGE RECTO 17.png");
+    effect17.classList.remove("animate__flip");
+    effect17.classList.add("animate__flip");
+  } else {
+    effect17.setAttribute("src", "assets/img/VERSO 17.png");
+    effect17.classList.remove("animate__flip");
+    effect17.classList.add("animate__flip");
+  }
+};
+
+let effect24 = document.getElementById("effect24");
+
+effect24.onclick = () => {
+  if (effect24.classList.contains("animate__flip")) {
+    effect24.setAttribute("src", "assets/img/PAGE RECTO 24.png");
+    effect24.classList.remove("animate__flip");
+    effect24.classList.add("animate__flip");
+  } else {
+    effect24.setAttribute("src", "assets/img/VERSO 24.png");
+    effect24.classList.remove("animate__flip");
+    effect24.classList.add("animate__flip");
+  }
+};
+
+// animate__hinge
